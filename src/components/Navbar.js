6450b9543362bf7d67807186ae4ff6b84a48cf59
orcmane <yogeshgrover73@gmail.com>
2023-03-18 12:59:39 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import logo from '../images/logo.svg'
+import {pageLinks} from '../data'
+
+const Navbar = () => {
+  return (
+   <nav class="navbar">
+      <div class="nav-center">
+        <div class="nav-header">
+          <img src={logo} class="nav-logo" alt="backroads" />
+          <button type="button" class="nav-toggle" id="nav-toggle">
+            <i class="fas fa-bars"></i>
+          </button>
+        </div>
+ 
+        <ul class="nav-links" id="nav-links">
+          {pageLinks.map((link)=>{
+            return(
+          <li key={link.id}>
+            <a href={link.href} class="nav-link"> {link.text}</a>
+          </li>
+            )
+          })}
+          
+        </ul>
+
+        <ul class="nav-icons">
+          <li>
+            <a href="https://www.twitter.com" target="_blank" class="nav-icon">
+              <i class="fab fa-facebook"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com" target="_blank" class="nav-icon">
+              <i class="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com" target="_blank" class="nav-icon">
+              <i class="fab fa-squarespace"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const pageLinks=[
+    {id:1,href:'#home',text:'home'},
+    {id:2,href:'#about',text:'about'},
+    {id:3,href:'#services',text:'services'},
+    {id:4,href:'#tours',text:'tours'},
+];
+
+export const services=[
+    {id:1,
+      icon:'fas fa-wallet fa-fw',
+      title:'saving money',
+    text:'Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.',
+    
+    },
+    {id:2,
+      icon:'fas fa-wallet fa-fw',
+      title:'endless hiking',
+    text:'Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.',
+    
+    },
+    {id:3,
+      icon:'fas fa-wallet fa-fw',
+      title:'amazing comfort',
+    text:'Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.',
+    
+    },
+]
